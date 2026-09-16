b75e0762032bbd82ba865a092c0263e261e8b06b
tharindudhananjayaekanayaka-hub <tharindudhananjayaekanayaka@gmail.com>
2026-09-16 23:55:05 +0530

    chore: update entry point to initialize splash screen

--- mobile-app/lib/main.dart
+++ mobile-app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
+import 'screens/splash_screen.dart';
 
 void main() {
   runApp(const EventCraftApp());
@@ -18,7 +18,7 @@ class EventCraftApp extends StatelessWidget {
         scaffoldBackgroundColor: const Color(0xFF0F172A),
         primarySwatch: Colors.cyan,
       ),
-      home: const HomeScreen(),
+      home: const SplashScreen(),
     );
   }
 }
