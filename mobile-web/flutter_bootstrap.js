76c619085390322c771fde78b9732824a41c9ff0
IT24104333 <gayax000@gmail.com>
2026-09-17 10:55:06 +0530

    fix(mobile-cache): force purge stale mobile service workers, delete browser cache storage, and append dynamic timestamps to dart bundle

--- mobile-web/flutter_bootstrap.js
+++ mobile-web/flutter_bootstrap.js
@@ -6,11 +6,8 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba9960f","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
-
+_flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba9960f","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=" + new Date().getTime()}]};
 
 _flutter.loader.load({
-  serviceWorkerSettings: {
-    serviceWorkerVersion: "400714979"
-  }
+  serviceWorkerSettings: null
 });
